869da3732bae9cbd64d371cfd6ee0afa4dcffada
Lonnie Coffman <lonniecoffman@icloud.com>
2016-10-14 17:26:52 -0700

    cleaned up TTT bonus

--- course_101/assignments/tictactoe_bonus.rb
+++ course_101/assignments/tictactoe_bonus.rb
@@ -7,7 +7,7 @@ COMPUTER_MARKER = 'O'.freeze
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
                 [[1, 5, 9], [3, 5, 7]]
-NUM_GAMES = 3
+NUM_GAMES = 5
 
 def prompt(message)
   puts "=>#{message}"
