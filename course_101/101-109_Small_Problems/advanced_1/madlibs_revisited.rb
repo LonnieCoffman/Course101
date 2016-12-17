f0c8f6a0cba0b7f4396cbaf7c01d1c04fef4f917
LonnieCoffman <lonniecoffman@icloud.com>
2016-12-17 09:01:28 -0800

    advanced wip

---
+++ course_101/101-109_Small_Problems/advanced_1/madlibs_revisited.rb
@@ -0,0 +1,8 @@
+NOUNS = %w(adult derby kitchen squirrel team
+           trailer bookcase cabin gas epoxy).freeze
+VERBS = %w(remind command shop explain examine
+           delight smoke spill sail concentrate).freeze
+ADJECTIVES = %w(small boundless colossal excited fluffy
+                snobish tense grouchy narrow witty).freeze
+ADVERBS = %w(repeatedly politely happily mockingly sleepily
+             sweetly seldom absentmindedly tenderly freely).freeze
