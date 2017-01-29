135648fa8b47197023765aabba860a4937f6971d
lonniecoffman <lonniecoffman@icloud.com>
2017-01-29 12:41:55 -0800

    oop basics wip

---
+++ 120_object_oriented_programming/oo_basics-inheritance/5_towable_pt1.rb
@@ -0,0 +1,19 @@
+# Module: Towable
+# -- Method: tow
+# ----print "I can tow a trailer!"
+
+module Towable
+  def tow
+    "I can tow a trailer!"
+  end
+end
+
+class Truck
+  include Towable
+end
+
+class Car
+end
+
+truck1 = Truck.new
+puts truck1.tow
